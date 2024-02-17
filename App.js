6f193320aa84607696f3d7988ab0eacef0af8df4
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-02-18 00:39:59 +0800

    added a stack navigator

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/splash';
+import DataPrivacyScreen from './src/screens/dataprivacy';
+import HomeScreen from './src/screens/home';
+
+const stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen
+            name={'SplashScreen'}
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+          <stack.Screen
+            name={'DataPrivacyScreen'}
+            component={DataPrivacyScreen}
+            options={{headerShown: false}}
+          />
+          <stack.Screen
+            name={'HomeScreen'}
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
