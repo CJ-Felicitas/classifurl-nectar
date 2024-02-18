f3d1d11e7fbde5e4f2a799be68e0edc8e076b674
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-02-19 04:56:29 +0800

    Android: Added the native module for TextDetectionPackage

--- android/app/src/main/java/com/classifurl/MainApplication.kt
+++ android/app/src/main/java/com/classifurl/MainApplication.kt
@@ -11,16 +11,18 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.classifurl.TextDetectionPackage
+import com.facebook.react.shell.MainReactPackage
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+        override fun getPackages(): List<ReactPackage> {
+          val packages: MutableList<ReactPackage> = PackageList(this).packages.toMutableList()
+          packages.add(TextDetectionPackage()) // Add TextDetectionPackage here
+          return packages
+        }
 
         override fun getJSMainModuleName(): String = "index"
 
