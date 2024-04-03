6d24d37f4a534a4d41ac1e6a20a1bcba47e7fbdd
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-03 10:06:47 +0800

    updated the postData function

--- src/screens/paste/index.js
+++ src/screens/paste/index.js
@@ -6,43 +6,31 @@ export default function PasteUrl({navigation}) {
     const [url, setUrl] = useState('')
     const [reply, setReply] = useState('')
     const [urlsubmit, setUrlSubmit] = useState('')
-    const postData = () => {
-    fetch('http://74.226.249.87:3000/api/submiturl', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // Specify content type
-      },
-      body: JSON.stringify({
-        url: url
-      }), // Convert data to JSON string
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok (${response.status} - ${response.statusText})`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Data received:', data);
-        setReply(data.message)
-        setUrlSubmit(data.data);
-        // Handle the retrieved data here
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error.message);
-        // Handle errors here
-        if (error.response) {
-          // The request was made and the server responded with a non-2xx status code
-          console.error('Server responded with:', error.response.data);
-          console.error('Headers:', error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.error('No response received. Request details:', error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.error('Error details:', error.message);
-        }
-      });
+
+    const postData = async () => {
+      try {
+          const response = await fetch('http://10.0.254.14:3000/api/submiturl', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  url: url
+              }),
+          });
+          
+          if (!response.ok) {
+              throw new Error(`Network response was not ok (${response.status} - ${response.statusText})`);
+          }
+  
+          const data = await response.json();
+          console.log('Data received:', data.data);
+          console.log('Server response', data.flask);
+          // setReply(data.data)
+          setUrlSubmit(data.data);
+      } catch (error) {
+          console.error('Error fetching data:', error.message);
+      }
   };
 
   return (
@@ -54,7 +42,7 @@ export default function PasteUrl({navigation}) {
       style={styles.input}
       />
       <Button onPress={postData} style={styles.fixToText} title='Submit URL' />
-      <Text style={styles.title}>{reply}</Text>
+      <Text style={styles.title}>temporary null return</Text>
       <Text style={styles.title}>the url you sent is "{urlsubmit}"</Text>
     
       </View>
